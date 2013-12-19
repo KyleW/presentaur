57c3ee38134cfa665a9eec4c733b8f95c50571eb
KyleW <Kyle.Warneck@gmail.com>
2013-12-19 15:04:22 -0800

    initial version of db functions

--- dbHelpers.js
+++ dbHelpers.js
@@ -3,43 +3,70 @@
 //docs are here: http://mongodb.github.io/node-mongodb-native/api-articles/nodekoarticle1.html
 var MongoClient = require('mongodb').MongoClient;
 
+var db;
 
 // Connect to the db
-MongoClient.connect("mongodb://localhost:27017/presDb", function(err, db) {
+
+MongoClient.connect("mongodb://localhost:27017/presDb", function(err, presDb) {
   if(err) {
     console.log("Something went wrong while trying to connect.");
   } else{
     console.log("We are connected");
   }
 
+  db = presDb;
+
   //Creates collections
-  db.createCollection('meetings', function(err, collection) {});
-  db.createCollection('users', function(err, collection) {});
+  db.createCollection('meetings', function(err, collection) {
+    if(err){console.log("error creating meetings collection: ", err);}
+  });
+  db.createCollection('users', function(err, collection) {
+    if(err){console.log("error creating meetings collection: ", err);}
+  });
 
 });
 
+var checkConnection = function(){
+  // if connected return true
+  // else open connection
 
-
+  return true; // stub for code to handle a timed out connection;
+};
 
 
 // Manage Meetings
+
+
+module.exports.createMeeting = function(meetingName){
+  checkConnection();
+
+  var doc = {meetingName: meetingName};
+
+  db.collection('meetings').insert(doc, {w:1}, function(err, result) {
+    if(err){
+      console.log("Insert failed: ", err);
+    } else {
+      console.log("new meeting created");
+    }
+  });
+};
+
+module.exports.updateMeeting = function(meetingId,doc){
+  checkConnection();
+  db.collection('meetings').update({meetingId:meetingId}, doc, {w:1}, function(err, result) {
+    if(err){
+      console.log(err);
+    } else {
+      console.log("Meeting updated.");
+    }
+  });
+};
+
+
 module.exports.getMeeting = function(meetingId){
+  checkConnection();
   return presDb.meetings.findOne({_id: meetingId});
 };
-
-module.exports.createMeeting=function(meetingName){
-  return PresDb.meetings.findAndModify( {
-    query: { meetingName: "Andy" },
-    sort: { rating: 1 },
-    update: { $inc: { score: 1 } },
-    upsert: true
-  });//what information do we have at insert? assuming nothing
-};
-
-module.exports.updateMeeting = function(){
-
-};
-
 
 
 
@@ -47,10 +74,12 @@ module.exports.updateMeeting = function(){
 // Speaker List
 
 module.exports.getSpeakerList = function(meetingNumber){
+  checkConnection();
 
 };
 
 module.exports.setSpeakerList = function(meetingNumber){
+  checkConnection();
 
 };
 
@@ -59,13 +88,16 @@ module.exports.setSpeakerList = function(meetingNumber){
 // finding meetings associated with users
 
 module.exports.findMeetingBySpeaker = function(){
+  checkConnection();
 
 };
 
 module.exports.findMeetingByOwner = function(){
+  checkConnection();
+
 
 };
 
 module.exports.findMeetingByOwnerOrSpeaker = function(){
-
+  checkConnection();
 };
