d09beb6e8d8ceb61febb614714bb3361ea6465c3
KyleW <Kyle.Warneck@gmail.com>
2013-12-19 09:22:50 -0800

    start on db stuff

---
+++ dbSchema.js
@@ -0,0 +1,11 @@
+/* the DB is called presDB
+meeting documents go in a collection called meetings (presDB.meetings)
+user documents go in a collection called users (presDB.users)
+ 
+
+Meeting Documents take the following form:
+ {
+
+}
+
+*/
