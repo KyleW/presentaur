4f0ec519c287527177d4c6b93ae6e08b8c34c4dd
ehannum <ehannum@gmail.com>
2013-12-18 22:04:49 -0800

    added angular-route.js and began work on routing.

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,22 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', [])
+.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider.when('/', {
+    controller: 'LoginController',
+    templateUrl: 'templates/login.html'
+  }).when('/signup', {
+    controller: 'LoginController'
+
+  }).when('/dj', {
+    controller: 'LoginController'
+
+  }).when('/present', {
+    controller: 'LoginController'
+
+  }).when('/404', {
+    templateUrl: 'templates/404.html'
+  })
+  .otherwise({redirectTo: '/404'});
+}])
 
 app.run(function($rootScope){
   $rootScope.name = "Presentaur"
--- client/src/controllers.js
+++ client/src/controllers.js
@@ -1,3 +1,15 @@
-app.controller('loginController', ['$scope', function ($scope) {
+app.controller('LoginController', ['$scope', function ($scope) {
   $scope.name = 'Create New Meeting'
+}])
+
+app.controller('SubmitController', ['$scope', function ($scope) {
+  $scope.name = 'Sign Up For Meeting'
+}])
+
+app.controller('DashboardController', ['$scope', function ($scope) {
+  $scope.name = 'DJ Dashboard'
+}])
+
+app.controller('PresenterController', ['$scope', function ($scope) {
+  $scope.name = 'Presentation View'
 }])
