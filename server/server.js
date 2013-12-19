c7f75df6afd54540663bbdc5b9c09e5f68dda4e2
Brannon Lucas <brannonlucas@gmail.com>
2013-12-18 23:03:23 -0800

    basic server ready

--- server/server.js
+++ server/server.js
@@ -1,17 +1,12 @@
-var app = require('express')()
-var server = require('http').createServer(app)
-var io = require('socket.io').listen(server);
+var express = require('express');
+var url = require('url');
+var server = express();
+
+
+
+server.get('/', function (req, res) {
+  console.log(req.route)
+  res.sendfile(url.resolve(__dirname, './client/index.html'));
+});
 
 server.listen(3000);
-
-app.get('/', function (req, res) {
-  console.log(req.route)
-  res.sendfile(__dirname + '../index.html');
-});
-
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
