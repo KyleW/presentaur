7758b2ca2dfc58af37ec707ffa6eb5f6cf83070f
Brannon Lucas <brannonlucas@gmail.com>
2013-12-20 11:53:19 -0800

    presentation and user helper files added

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
-// require('./initializeDB.js')();
+// require('./initializeDB.js')();  ------Is this still needed?
 var express = require('express');
 var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var url = require('url');
 var path = require('path');
+
+// Server and DB helpers
 var dbHelpers = require('./dbHelpers.js');
 var meeting = require('./meetingHelpers');
-
+var user = require('./userHelpers');
+var presentation = require('./presentationHelpers');
 var route = require('./router.js');
 
 server.listen(3000);
@@ -38,13 +41,13 @@ app.get('/meeting/:id', meeting.get);
 
 
 // Users
-app.post('/user/new', function(req,res){
+app.post('/user/new', user.create);
 
-});
+app.get('user/:id', user.get);
 
-app.get('user/:id', function(req, res){
+// Presentations
 
-});
+app.post('/presentation/new', presentation.create);
 
-
+app.get('/presentation/:id', presentation.get);
 
