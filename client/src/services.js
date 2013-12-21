469c3a2b9441303cea8bbdb2d1d8b855dea6bfb3
ehannum <ehannum@gmail.com>
2013-12-21 12:51:28 -0800

    debugging GET requests for existing meetings

--- client/src/services.js
+++ client/src/services.js
@@ -4,8 +4,9 @@ app.factory('sharedMethods', [function () {
 
   // placeholder for testing purposes - !!! - BROKEN AND STUPID!
   // var presenterQueue = [{name:'Bones Malone', urls:['http://www.farm2.me', 'http://www.youtube.com']}, {name:'Spiderman', urls:['http://www.spid.er/man']}, {name:'Eric Hannum', urls:['http://www.dook.ly']}];
-  var meeting = {_id: '5', meetingName:'The Example Meeting', speakers:[{name:'Bones Malone', url:'http://slid.es/erichannum/organized-crime'}, {name:'Spiderman', url:'http://www.spid.er/man'}, {name:'Eric Hannum', url:'http://www.dook.ly'}]};
-
+  // var meeting = {_id: '5', meetingName:'The Example Meeting', speakers:[{name:'Bones Malone', url:'http://slid.es/erichannum/organized-crime'}, {name:'Spiderman', url:'http://www.spid.er/man'}, {name:'Eric Hannum', url:'http://www.dook.ly'}]};
+  var meeting;
+  
   return {
     updateQueue: function (presenters) {
       meeting.speakers = presenters;
