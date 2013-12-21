97e854cc41aedec29742dc1578fb384cb547a1fb
KyleW <Kyle.Warneck@gmail.com>
2013-12-21 13:51:11 -0800

    added skeleton for auth and passport node module

---
+++ server/auth.js
@@ -0,0 +1,14 @@
+var passport = require('passport');
+var GoogleStrategy = require('passport-google').Strategy;
+
+
+passport.use(new GoogleStrategy({
+    returnURL: 'http://www.example.com/auth/google/return',
+    realm: '/' //'http://www.example.com/' 
+  },
+  function(identifier, profile, done) {
+    User.findOrCreate({ openId: identifier }, function(err, user) {
+      done(err, user);
+    });
+  }
+));
