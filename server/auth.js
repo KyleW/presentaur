d1a57904ff6e8fef5bc4e0e256c2a866b52bc073
KyleW <Kyle.Warneck@gmail.com>
2014-01-06 15:48:59 -0800

    master was broken. There were stubs for userHelpers, but no user helper functions. I added placeholders to resolve the issue.

--- server/auth.js
+++ server/auth.js
@@ -3,13 +3,16 @@ var GoogleStrategy = require('passport-google').Strategy;
 
 
 passport.use(new GoogleStrategy({
-    returnURL: 'http://www.example.com/auth/google/return',
+    returnURL: 'http://presentaur.herokuapp.com/auth/google/return',
     realm: 'http://presentaur.herokuapp.com/'
   },
   function(identifier, profile, done) {
+    //Use identifier to find or create the user
     User.findOrCreate({ openId: identifier }, function(err, user) {
       done(err, user);
     });
+
+    //grab what we want from profile and store it
   }
 ));
 
