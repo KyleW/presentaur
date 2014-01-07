563d42bc2d2ecad33118e1f6bc6b45c32a48b37b
KyleW <Kyle.Warneck@gmail.com>
2014-01-07 15:01:45 -0800

    removed a bunch of console logs

--- server/userHelpers.js
+++ server/userHelpers.js
@@ -5,13 +5,10 @@ var dbHelpers = require('./dbHelpers.js');
 module.exports = {
 
   findOne:function(user , callback){
-    console.log("retrieving user with criteria: ", user);
-
     dbHelpers.db.collection('users',function(err,collection){
       collection.find(user).toArray(function(err,result){
-        if(err) {console.log("Looking for a user and failed failed ",err);}
+        if(err) {console.log("Looking for a user and failed ",err);}
         else {
-          console.log("Found the user you're looking for", result);
           callback(err, result[0]);
         }
       });
@@ -19,13 +16,10 @@ module.exports = {
   },
 
   findById:function(id , callback){
-    console.log("retrieving user with criteria: ", id);
-
     dbHelpers.db.collection('users',function(err,collection){
       collection.find({_id:new BSON.ObjectID(id)}).toArray(function(err,user){
-        if(err) {console.log("Looking for a user and failed failed ",err);}
+        if(err) {console.log("Looking for a user and failed ",err);}
         else {
-          console.log("Found the user you're looking for", user);
           callback(err, user[0]);
         }
       });
@@ -37,14 +31,11 @@ module.exports = {
     var password = req.body.password;
     var doc = {'username': username, 'password': password};
 
-    console.log('Adding user with username: ' + doc.username);
-
     dbHelpers.db.collection('users', function (err, collection){
       collection.save(doc, {w:1}, function (err, result) {
         if(err){
           console.log("Insert failed: ", err);
         } else {
-          console.log('The user named: ' + result.username + ' has been assigned the id: ' + result._id);
           res.send(JSON.stringify(result));
         }
       });
@@ -53,7 +44,6 @@ module.exports = {
 
   findOrCreate: function(user,callback){
     dbHelpers.db.collection('users',function(err,collection){
-      console.log("looking for . . . " + user.openId);
       collection.find({openId: user.openId}).toArray(function(err,result){
         if(err) {console.log("Looking for a user and failed failed ",err);}
         else {
