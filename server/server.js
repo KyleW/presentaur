6f2e7d3d8fdca3426c5a4b6b46b196424f40a32c
KyleW <Kyle.Warneck@gmail.com>
2014-01-06 17:22:51 -0800

    a clean start on auth

--- server/server.js
+++ server/server.js
@@ -70,14 +70,3 @@ app.get('user/:id', user.get);
 
 
 // Auth
-
-// Redirect the user to Google for authentication.  When complete, Google will redirect the user back to the application at /auth/google/return
-app.get('/auth/google', passport.authenticate('google'));
-
-// Google will redirect the user to this URL after authentication.  Finish
-// the process by verifying the assertion.  If valid, the user will be
-// logged in.  Otherwise, authentication has failed.
-app.get('/auth/google/return',
-  passport.authenticate('google', { successRedirect: '/',
-                                    failureRedirect: '/login' }));
-};
