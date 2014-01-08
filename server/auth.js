10ae4c378297f52ffac7216fcdf0ea2f25200c04
KyleW <Kyle.Warneck@gmail.com>
2014-01-07 17:01:03 -0800

    normalized profile data and changed urls to deployed urls

--- server/auth.js
+++ server/auth.js
@@ -34,13 +34,20 @@ passport.use(new LocalStrategy(
 
 
 passport.use(new GoogleStrategy({
-    returnURL: 'http://localhost:5000/auth/google/return',
-    realm: 'http://localhost:5000'
-    // returnURL: 'http://presentaur.herokuapp.com/auth/google/return',
-    // realm: 'http://presentaur.herokuapp.com'
+    // returnURL: 'http://localhost:5000/auth/google/return',
+    // realm: 'http://localhost:5000'
+    returnURL: 'http://presentaur.herokuapp.com/auth/google/return',
+    realm: 'http://presentaur.herokuapp.com'
   },
   function(identifier, profile, done) {
-    User.findOrCreate(profile, function(err, user) {
+    var newUser = {
+      displayName: profile.displayName,
+      name: profile.name,
+      email: profile.emails[0].value,
+      headline: null,
+      pictureUrl: null
+    };
+    User.findOrCreate(newUser, function(err, user) {
       return done(err, user);
     });
   }
@@ -50,13 +57,19 @@ passport.use(new GoogleStrategy({
 passport.use(new LinkedInStrategy({
     consumerKey: Config.LINKEDIN_API_KEY,
     consumerSecret: Config.LINKEDIN_SECRET_KEY,
-    callbackURL: "http://localhost:5000/auth/linkedin/return",
-    // callbackURL: "http://presentaur.herokuapp.com/auth/linkedin/return",
+    // callbackURL: "http://localhost:5000/auth/linkedin/return",
+    callbackURL: "http://presentaur.herokuapp.com/auth/linkedin/return",
     profileFields: ['id', 'first-name', 'last-name', 'email-address', 'headline','picture-url']
   },
   function(token, tokenSecret, profile, done) {
-    console.log(profile);
-    User.findOrCreate(profile, function (err, user) {
+    var newUser = {
+      displayName: profile.displayName,
+      name: profile.name,
+      email: profile.emails[0].value,
+      headline: profile._json.headline,
+      pictureUrl: profile._json.pictureUrl
+    };
+    User.findOrCreate(newUser, function (err, user) {
       return done(err, user);
     });
   }
