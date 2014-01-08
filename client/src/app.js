8a2f915f36c60f7cf9b79fd177c0c0da01faee59
ehannum <ehannum@gmail.com>
2014-01-08 12:41:48 -0800

    Added signing in for presenters, ready for database updating accordingly

--- client/src/app.js
+++ client/src/app.js
@@ -26,17 +26,15 @@ var app = angular.module('myApp', ['ngRoute', 'ngCookies', 'btford.socket-io'])
   }).when('/present/:id', {
     controller: 'PresentController',
     templateUrl: 'templates/present.html'
-  }).when('/404', {
-    templateUrl: 'templates/404.html'
   }).when('/newUser', {
     templateUrl: 'templates/createUser.html'
   })
-  .otherwise({redirectTo: '/new'});
+  .otherwise({redirectTo: '/'});
 }])
 
 .run(function ($rootScope, $cookies, $cookieStore, $http, $location) {
   $rootScope.id = '';
-  $rootScope.userid = $cookieStore.get('userid') || '';
+  $rootScope.userid = $cookieStore.get('userid') || null;
   $rootScope.loggedIn = false;
   if (!!$rootScope.userid) {
     $http({
@@ -55,7 +53,7 @@ var app = angular.module('myApp', ['ngRoute', 'ngCookies', 'btford.socket-io'])
   $rootScope.logout = function () {
     $cookieStore.remove('userid');
     $location.url('/');
-  $rootScope.userid = '';
-  $rootScope.loggedIn = false;
+    $rootScope.userid = '';
+    $rootScope.loggedIn = false;
   };
 });
